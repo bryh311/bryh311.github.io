c239f9eb4df7a7b93950a871b7f85245939c76af
Bryce Hill <bryh311@gmail.com>
2025-11-11 21:08:59 -0500

    add github workflow setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bryh311.github.io",
+  base: "/",
   markdown: {
     shikiConfig: {
       theme: "everforest-light",
